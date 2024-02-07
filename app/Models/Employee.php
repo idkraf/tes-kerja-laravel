a334838a8f68c53a29e9ea3d946436f0f2128cbe
wanss <mohbaswandegorie@gmail.com>
2024-02-07 12:02:28 +0700

    perbaikan view, database migration.

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function bank()
+    {
+        return $this->belongsTo(Bank::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
 }
--- database/migrations/2024_02_07_032739_create_employees_table.php
+++ database/migrations/2024_02_07_032739_create_employees_table.php
@@ -15,6 +15,21 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('ktp');
+            $table->string('date_birth');
+            $table->string('phone');
+            $table->string('province_id');
+            $table->string('city_id');
+            $table->string('address');
+            $table->string('kodepos');
+            $table->string('bank_id');
+            $table->string('bank_number');
+            
+
             $table->timestamps();
         });
     }
